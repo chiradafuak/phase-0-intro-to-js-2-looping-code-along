a3ebab91616cb8c002ff44857ad5c7d8f666596d
Ian <ian.githae2@student.moringaschool.com>
2025-03-25 15:06:02 +0300

    Looping lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solutions in this file
+function countDown(num) {
+    while (num >= 0) {
+      console.log(num);
+      num--;
+    }
+  }
+  
+  function writeCards(names, event) {
+    const messages = [];
+    let i = 0;
+    
+    while (i < names.length) {
+      messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+      i++;
+    }
+    
+    return messages;
+  }
+  
+  console.log("Countdown from 10:");
+  countDown(10);
+  
+  console.log("\nThank you cards:");
+  const thankYouMessages = writeCards(["Charlie", "Samip", "Ali"], "birthday");
+  console.log(thankYouMessages);
